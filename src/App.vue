5444ba127533b376c757d0cc49f6e48ffe546694
nazareth Astudillo <99359614+nazastudillo@users.noreply.github.com>
2022-05-04 15:40:58 -0700

    changes to nav bar and content

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <nav>
+    <img src="@/assets/HKLogo.png" alt="" />
+
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
@@ -18,13 +20,26 @@
 }
 
 nav {
-  padding: 30px;
+  padding: 15px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+nav img {
+  width: 50px;
+  height: 50px;
+  justify-content: left;
+  position: absolute;
 }
 
 nav a {
   font-weight: bold;
+  font-size: 22px;
   color: #2c3e50;
   text-decoration: none;
+  position: relative;
+  float: none;
 }
 
 nav a.router-link-exact-active {
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,46 +1,138 @@
 <template>
-  <div class="about">
-    <h1>We are Hello Kittea</h1>
+  <div class="name">We are: Hello Kittea</div>
 
-    <p>Our mission is to create high quality and fresh drinks for everyone.</p>
-    <p>
-      We constantly update our menu with new flavors and combinations, so that
-      people have a chance to try something new every time they come in.
-    </p>
-    <p>
-      We want to reduce as much waste as possible, so we use bio-degradable
-      materials.
-    </p>
-    <p>
-      Part of our proceeds go to non-profit organizations that clear the ocean
-      of waste.
-    </p>
+  <div class="media">
+    <div class="image-block">
+      <img src="../assets/bubble2.jpg" alt="" class="boba2" />
+    </div>
+
+    <div class="about">
+      <p>
+        Our mission is to create high quality and fresh drinks for everyone.
+      </p>
+      <p>
+        We aren't just a bubble tea shop, were a place for everyone to come
+        together and enjoy themselves.
+      </p>
+      <p>
+        We constantly update our menu with new flavors and combinations, so that
+        you get a chance to try something new every time you come in!
+      </p>
+      <p>
+        Our tea is freshly brewed in house and we aim to make every cup perfect.
+        If it is not to your liking we will always re-make it for you.
+      </p>
+    </div>
   </div>
 
-  <div class="image-block"></div>
+  <div class="statement">
+    <div class="message">
+      <p>
+        We care about environment. We take pride in using eco-friendly and
+        recycled material. That's not all!! Part of our proceeds go towards
+        non-profit organizations that help out the planet.
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
 </script>
 
-<style>
+<style lang="scss">
 @import url(//fonts.googleapis.com/css?family=Lato:300:400);
 
-h1 {
+.name {
+  /* From https://css.glass */
+  background: rgba(255, 255, 255, 0.25);
+  border-radius: 15px;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(8.5px);
+  -webkit-backdrop-filter: blur(8.5px);
   font-family: "Lato", sans-serif;
   font-weight: 300;
+  font-size: 48px;
   letter-spacing: 2px;
-  font-size: 64px;
 }
 
-p {
-  font-family: "Lato", sans-serif;
-  letter-spacing: 1px;
-  font-size: 16px;
-  color: #333333;
+.boba2 {
+  padding: 20px;
+  margin: 100px;
+  width: 350px;
+  height: auto;
+  @media (min-width: "1080px") {
+    width: 500px;
+  }
+  @media (min-width: "600px") {
+    width: 425px;
+  }
 }
 
-.about {
-  position: flex;
+.media {
+  overflow: hidden;
+  position: relative;
+  top: -45px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  p {
+    font-size: 14px;
+    max-width: 350px;
+  }
+  @media (min-width: "600px") {
+    .about {
+      p {
+        font-size: 18px;
+        max-width: 425px;
+      }
+    }
+  }
+  @media (min-width: "1080px") {
+    .about {
+      p {
+        position: relative;
+        left: -20px;
+        font-size: 22px;
+      }
+    }
+  }
+}
+
+.statement {
+  overflow: hidden;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  p {
+    font-size: 14px;
+    max-width: 350px;
+  }
+  @media (min-width: "600px") {
+    .message {
+      p {
+        font-size: 18px;
+        max-width: 425px;
+      }
+    }
+  }
+  @media (min-width: "1080px") {
+    .message {
+      p {
+        position: relative;
+        left: -20px;
+        font-size: 22px;
+        max-width: 750px;
+      }
+    }
+  }
+}
+
+@media screen and (max-width: 1080px) {
+  .media {
+    flex-direction: column;
+    align-content: center;
+    justify-content: center;
+  }
 }
 </style>
